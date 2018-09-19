5fc507bfc9549c064648170def19a34654c734c7
wangfan <1203563384@qq.com>
2018-09-19 18:55:53 +0800

    返回标准格式数据

--- client/client.php
+++ client/client.php
@@ -1,4 +1,15 @@
 <?php  
-	
+	include './vendor/autoload.php';
+
+	$url = 'http://localhost/Api/api_one/API_test/server/server.php';
+
+	$curl = new Curl\Curl();
+	$curl->get($url);
+
+	if($curl->error){
+		echo $curl->error_code;
+	}else{
+		echo $curl->response;
+	}
 
 ?>
--- server/server.php
+++ server/server.php
@@ -1,3 +1,19 @@
 <?php  
-	
+	$pdo = new PDO('mysql:host=localhost;dbname=api;chraset=utf8','root','111');
+
+	$stmt = $pdo->query('select * from user ');
+
+	$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	function resp($data,$status,$message){
+		$res = [
+			'status'=>$status,
+			'message'=>$message,
+			'data'=>$data
+		];
+
+		return json_encode($res,JSON_UNESCAPED_UNICODE);
+	}
+
+	echo resp($data,200,'ok');
 ?>
